40d70728d228e2fd2c8994f4063c8f2b7a4ac6f5
tarekul42 <tarekulrifat205@gmail.com>
2025-04-30 23:35:01 +0600

    reference types

---
+++ module1/src/referenceType.ts
@@ -0,0 +1,14 @@
+// Object
+const user: {
+  readonly company: "technology studies and computer"; // type --> literal types
+  firstName: string;
+  middleName?: string; //optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "technology studies and computer",
+  firstName: "Tarekul",
+  //   middleName: "Islam",
+  lastName: "Rifat",
+  isMarried: false,
+};
