bf2a75aa172158af85a9e51751e42d756bd24356
tarekul42 <tarekulrifat205@gmail.com>
2025-05-02 12:35:45 +0600

    nullable types

---
+++ module1/src/nullableTypes.ts
@@ -0,0 +1,17 @@
+{
+    // 
+
+    const searchName = (value: string | null) =>{
+        if(value) {
+            console.log("Searching")
+        } else {
+            console.log("There is nothing to search")
+        }
+    }
+
+    searchName(null)
+
+
+
+    // 
+}
