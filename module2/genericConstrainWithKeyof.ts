7803eb0b38f9eaa82f0579d7ff909df20b8adead
tarekul42 <tarekulrifat205@gmail.com>
2025-05-02 22:21:23 +0600

    generic constrains with keyof operator

--- module2/genericConstrainWithKeyof.ts
+++ module2/genericConstrainWithKeyof.ts
@@ -12,5 +12,21 @@
 
   type Owner2 = keyof Vehicle;
 
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+
+  const user = {
+    name: "Forashi",
+    age: 83,
+    address: "Nigeria",
+  };
+
+  const car = {
+    model: "BMW i8",
+    year: 2000,
+  };
+
+  const result1 = getPropertyValue(car, "model");
   //
 }
