9998cbae4d81d3c7b45ebfb68598fd9fd78874fa
tarekul42 <tarekulrifat205@gmail.com>
2025-05-07 23:54:45 +0600

    task 4 and 5 done

--- FinalTask/FinalTask.ts
+++ FinalTask/FinalTask.ts
@@ -28,5 +28,41 @@
 
   filterByRating(books);
 
+  //   task 3
+
+  // task 4
+
+  class Vehicle {
+    constructor(private make: string, private year: number) {}
+    getInfo() {
+      return `Make: ${this.make}, Year: ${this.year}`;
+    }
+  }
+
+  class Car extends Vehicle {
+    constructor(make: string, year: number, private model: string) {
+      super(make, year);
+    }
+    getModel() {
+      return `Model: ${this.model}`;
+    }
+  }
+
+  const myCar = new Car("Toyota", 2020, "Corolla");
+  myCar.getInfo();
+  myCar.getModel();
+
+  //   task 5
+  function processValue(value: string | number): number {
+    if(typeof value === "string"){
+        return value.length;
+    } else {
+        return value*2;
+    }
+  }
+
+  console.log(processValue("bangladesh"))
+  console.log(processValue(10))
+
   //
 }
